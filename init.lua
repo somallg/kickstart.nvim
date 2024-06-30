98a2eb4e94aa299fac4ba41fc647bde69a03d31d
somallg <tq.duong@icloud.com>
2024-07-01 00:23:07 +0700

    configure treesitter based folding

--- init.lua
+++ init.lua
@@ -154,6 +154,12 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- Configure Treesitter based Folding
+vim.opt.foldmethod = 'expr'
+vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 99
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
