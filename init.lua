5c0c0808fd67c6b4386b5ef19cb891c541b21a30
somallg <tq.duong@icloud.com>
2024-07-30 00:48:55 +0700

    add save file keybinding

--- init.lua
+++ init.lua
@@ -196,6 +196,9 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Keybinds to save file
+vim.keymap.set({ 'i', 'x', 'n', 's' }, '<C-s>', '<cmd>w<CR><Esc>', { desc = 'Save File' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
