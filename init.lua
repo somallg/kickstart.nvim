999474a33458b55081a87d75227627b8a73435cd
somallg <tq.duong@icloud.com>
2024-10-02 12:59:33 +0700

    autocmd: add reset cursor on vim exit

--- init.lua
+++ init.lua
@@ -204,6 +204,13 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Reset cursor on quit
+vim.api.nvim_create_autocmd('VimLeavePre', {
+  desc = 'Reset cursor back to beam when leaving Neovim',
+  group = vim.api.nvim_create_augroup('somallg-reset-cursor-on-exit', { clear = true }),
+  command = 'set guicursor=a:ver25',
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
