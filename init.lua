a4c726e4c36816d30423bfe082def0f785653029
somallg <tqduong86@gmail.com>
2025-03-02 22:24:11 +0700

    add ts-comments.nvim

--- init.lua
+++ init.lua
@@ -916,6 +916,9 @@ require('lazy').setup({
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
+  -- Fix 'commentstring' is empty
+  { 'folke/ts-comments.nvim', event = 'VeryLazy', opts = {} },
+
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
     config = function()
