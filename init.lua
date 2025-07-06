74b48b2499702f261033a7ef8d82dea3d1d0021b
somallg <tqduong86@gmail.com>
2025-07-06 15:50:21 +0700

    feat(init.lua): configure Telescope to map 'q' to close action in normal mode

--- init.lua
+++ init.lua
@@ -418,15 +418,18 @@ require('lazy').setup({
 
       -- [[ Configure Telescope ]]
       -- See `:help telescope` and `:help telescope.setup()`
+      local actions = require 'telescope.actions'
+
       require('telescope').setup {
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
+        defaults = {
+          mappings = {
+            n = { ['q'] = actions.close },
+          },
+        },
+
         -- pickers = {}
         extensions = {
           ['ui-select'] = {
