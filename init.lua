ee32aae17e0eb41d17989ff95fb3f0bb0fd9602d
somallg <tqduong86@gmail.com>
2025-07-25 01:03:01 +0700

    feat: add keybindings

--- init.lua
+++ init.lua
@@ -211,6 +211,12 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
+-- Save file
+vim.keymap.set({ 'i', 'x', 'n', 's' }, '<C-s>', '<cmd>w<CR><Esc>', { desc = '[S]ave File' })
+
+-- Quit all
+vim.keymap.set('n', '<leader>qq', '<cmd>qa<CR>', { desc = '[Q]uit all' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
